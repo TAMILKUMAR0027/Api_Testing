b638060a39d7878452bbc2000a3994dd94696158
TAMILKUMAR0027 <tamilkumar0027@gmail.com>
2026-06-22 22:26:03 +0530

    added new valid testcases

--- Api_Testing_Practice/src/test/java/com/tests/GetAllNotesQP.java
+++ Api_Testing_Practice/src/test/java/com/tests/GetAllNotesQP.java
@@ -1,17 +1,74 @@
 package com.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class GetAllNotesQP extends BaseTest {
 
     @Test
-    public void getAllNotesUsingQueryParameter() {
+    public void getAllNotes_withoutQueryParams() {
+
         String token = getToken();
 
-        Response r = getAllNotes(token);
-        r.prettyPrint();
-        System.out.println("All notes Got based on filter results");
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get(baseURI + "getAll/notes")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        response.prettyPrint();
+
+        Assert.assertTrue(response.jsonPath().getBoolean("success"));
+        Assert.assertNotNull(response.jsonPath().getList("data"));
+    }
+
+    @Test
+    public void getAllNotes_withQueryParams() {
+
+        String token = getToken();
+
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .queryParam("page", data.getData("page"))
+                .queryParam("limit", data.getData("limit"))
+                .queryParam("sortOrder", data.getData("sortOrder"))
+                .when()
+                .get(baseURI + "getAll/notes")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        response.prettyPrint();
+
+        Assert.assertTrue(response.jsonPath().getBoolean("success"));
+    }
+
+    @Test
+    public void getAllNotes_withDifferentQueryParams() {
+
+        String token = getToken();
+
+        Response response = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .queryParam("page", 2)
+                .queryParam("limit", 1)
+                .queryParam("sortBy", "title")
+                .queryParam("sortOrder", "asc")
+                .when()
+                .get(baseURI + "getAll/notes")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        response.prettyPrint();
+
+        Assert.assertTrue(response.jsonPath().getBoolean("success"));
     }
 }
